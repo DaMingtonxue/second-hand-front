c9eff4dccbba772aefc675d04426d14113629b3d
小明同学 <10933562+xiao-mingdada@user.noreply.gitee.com>
2024-09-20 20:10:24 +0800

    前端部分

---
+++ src/views/manager/Api.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <div class="card" style="padding: 15px">
+        Api接口
+      </div>
+      <a href="http://localhost:9090/doc.html#/home" target="_blank">校园二手物品交易平台 API说明文档</a>
+            <div style="display: flex; grid-gap: 10px; margin: 10px 0">
+  
+      </div>
+    </div>
+  </template>
+  
+  <script>
+
+  </script>
